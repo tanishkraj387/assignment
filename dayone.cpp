c672fbe295591359cf28187320f0d4e32c8d375c
tanishkraj387 <tanishkraj387@gmail.com>
2026-06-01 20:34:48 +0530

    first commit

---
+++ dayone.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int sum=0;
+    cout<< "enter a number: ";
+    cin>>n;
+sum=n*(n+1)/2;
+cout<<"the sum of first "<<n<<" natural numbers is: "<<sum;
+    return 0;   
+}
